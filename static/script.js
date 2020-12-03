cef1cf7d79006078c48387825e2fa8cf47a86e2a
DominicWrege <dominic.wrege001@stud.fh-dortmund.de>
2020-12-03 14:36:33 +0100

    modal dialog and css

---
+++ static/script.js
@@ -0,0 +1,19 @@
+function showCustomAlter(message, type) {
+    const alertElement = document.querySelector("div#modal-alert");
+    if (alertElement) {
+        alertElement.textContent = message;
+        alertElement.hidden = false;
+        if (type === "error") {
+            alertElement.classList.add("color-error");
+        } else {
+            alertElement.classList.add("color-ok");
+        }
+        setTimeout(() => {
+            alertElement.hidden = !alertElement.hiiden;
+        }, 3000);
+    }
+}
+
+window.addEventListener("load", () => {
+    showCustomAlter("This is an Alert!", "ok");
+});
