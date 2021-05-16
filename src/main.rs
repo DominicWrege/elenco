b9f6dfa8d51f196016764eececb9f2d43c778ee1
DominicWrege <dominic.wrege001@stud.fh-dortmund.de>
2021-05-16 17:00:48 +0200

    exclude "static" path in logger

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn run() -> Result<(), anyhow::Error> {
         App::new()
             .data(state.clone())
             .wrap(middleware::Compress::default())
-            .wrap(Logger::new("ip: %a status: %s time: %Dms req: %r"))
+            .wrap(Logger::new("ip: %a status: %s time: %Dms req: %r").exclude_regex("^/static/"))
             .service(actix_files::Files::new("/static", "./static").show_files_listing())
             .route("/", web::get().to(|| util::redirect("/login")))
             .configure(routes::api)
